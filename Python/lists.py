224e77869b4f5171de9b7114ffd438f45491da27
Silviasfirstgit <silviaschmidt77@gmail.com>
2022-05-18 15:43:49 +0200

    another try

---
+++ Python/lists.py
@@ -0,0 +1,15 @@
+list1 = [1,2,3,3,4,5]
+list2 = [1.2,1.3,1.4,5,6]
+list3 = ["How", "are", "you", "today"]
+list4 = ["Fine", "Thanks", "for", "asking"]
+list5 = ["Only", 4, "you"]
+
+print(list1)
+print(type(list1))
+
+print(list5)
+print(type(list5))
+
+print(list3[3])
+
+print(list4)
